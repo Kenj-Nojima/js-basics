7d49593c2e3f2b72460180a67fdf383c421e9147
Inaki <binsss0911@gmail.com>
2025-05-04 16:53:50 +0800

    Data-Types

--- index.js
+++ index.js
@@ -7,4 +7,29 @@ let gender = "Male";
 console.log (message, name, age, gender);
 
 const PI = "3.14";
-console.log (PI);
+console.log (PI); 
+
+/* DATA TYPES */
+
+let messages = "This is string";
+let ages = 21;
+let isBoolean = true; 
+
+let person = {
+    names: "Kenji",
+    age: 21,
+
+};
+
+console.log(person["names"], person["age"]);
+
+let fruits = ["Apple", "Mango", "Banana"];
+
+console.log(fruits[2]);
+
+let add = function(a,b){
+    return a + b;
+}
+
+console.log(add(1, 2)); 
+
